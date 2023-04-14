be0c78a5e4c8fb66c63c025ccea10392bec557e4
Francisco Ramos <rfrancisco.dev@gmail.com>
2023-04-14 06:22:02 -0300

    install angular material & init ligth theme

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import { enableProdMode } from "@angular/core";
+import { enableProdMode, importProvidersFrom } from "@angular/core";
 import { environment } from "./environments/environment";
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideRouter } from "@angular/router";
 import { APP_ROUTES } from "./app/app.routes";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 if (environment.production) {
   enableProdMode();
@@ -12,5 +13,6 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
-  ]
+    importProvidersFrom(BrowserAnimationsModule)
+]
 }).catch(err => console.error(err));
